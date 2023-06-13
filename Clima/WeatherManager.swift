702d4b78014f5da3e8b304ad633c83fbaf0caec6
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-06-13 17:16:45 -0300

    update

--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -12,8 +12,37 @@ struct WeatherManager {
     let weatherURL  = "https://api.openweathermap.org/data/2.5/weather?appid=ecce665c459c1531208ad1a7e82a52c2&units=metric"
     
     func fetchWeather(cityName: String) {
-        let urlString = "\(weatherURL)&q=\(cityName)"
+        let cityString = cityName.replacingOccurrences(of: " ", with: "%")
+        print(cityString)
+        let urlString = "\(weatherURL)&q=\(cityString)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest (urlString: String) {
+        //1. Create URL
+        if let url = URL(string: urlString){
+            
+            //2. Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            //3. Give the session a task
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            
+            //4. Start the task
+            task.resume()
+        }
+    }
+    
+    func handle (data: Data?, response: URLResponse?, error: Error?){
+        if error != nil  {
+            print(error!)
+            return
+        }
         
-        print(urlString)
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+        
     }
 }
