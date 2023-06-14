3316e154fffaf96f082324d5cf39f63ae926685c
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-06-14 16:58:12 -0300

    update

--- Clima/ModelData.swift
+++ Clima/ModelData.swift
@@ -11,8 +11,14 @@ import Foundation
 struct WeatherData: Decodable {
     let name: String
     let main: Main
+    let weather: [Weather]
 }
 
 struct Main: Decodable {
     let temp: Double
+}
+
+struct Weather: Decodable{
+    let main: String
+    let description: String
 }
--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -47,7 +47,7 @@ struct WeatherManager {
         
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.main.temp)
+            print(decodedData.weather[0].description)
         } catch {
             print(error)
         }
